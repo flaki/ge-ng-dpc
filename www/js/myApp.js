4247382a0e7d9d5ce7e2be2f47a2bee1f9a7c129
Flaki <git@flaki.hu>
2016-11-15 15:36:55 +0100

    Service

--- www/js/myApp.js
+++ www/js/myApp.js
@@ -2,22 +2,34 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller("myController", myController);
 
+myApp.service("storageService", storageServiceMemory);
 
-function myController($scope) {
+
+function myController($scope, storageService) {
+  console.log(storageService);
   $scope.records = [];
   $scope.newRecord = {};
 
   $scope.addRecord = addRecord;
 
   function addRecord() {
-    $scope.records.push($scope.newRecord);
+    storageService.addRecord($scope.newRecord);
+    $scope.records = storageService.listRecords();
     $scope.newRecord = {};
   }
 }
 
-/*
-function addRecord() {
-$scope.records.push($scope.newRecord);
-$scope.newRecord = {};
+function storageServiceMemory() {
+  var storage = [];
+
+  function addRecord(newRecord) {
+    storage.push(newRecord);
+  }
+
+  function listRecords() {
+    return storage.slice();
+  }
+
+  this.addRecord = addRecord;
+  this.listRecords = listRecords;
 }
-*/
